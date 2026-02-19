9b6b843517214da55dd45a514bde870e0a250f0e
nepalprastut <nepalprastut02@gmail.com>
2026-02-19 11:23:25 +0545

    sql queries updated

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -5,8 +5,7 @@ CREATE TABLE users (
     role VARCHAR(20) NOT NULL CHECK (role IN ('OWNER', 'OFFICER', 'ADMIN'))
 );
 
-INSERT INTO users (username, password_hash, role)
-VALUES ('prastut', 'hashed_pw', 'OWNER');
+
 
 INSERT INTO users (username, password_hash, role)
 VALUES ('administrator', 'admin123', 'ADMIN');
@@ -25,11 +24,6 @@ CREATE TABLE vehicle_owners (
 );
 
 
-INSERT INTO vehicle_owners (user_id, full_name, citizenship_no, district, mobile_no)
-VALUES (1, 'Ram Bahadur', '123-456', 'Kathmandu', '98XXXXXXXX');
-
-INSERT INTO vehicle_owners (user_id, full_name, citizenship_no, district, mobile_no)
-VALUES (2, 'Hari Bahadur', '987-987', 'Lalitpur', '98XXXXXXXX');
 
 
 SELECT * FROM vehicle_owners;
@@ -43,11 +37,7 @@ CREATE TABLE officers (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-INSERT INTO users (username, password_hash, role)
-VALUES ('officer1', 'hashed_pw', 'OFFICER');
 
-INSERT INTO officers (user_id, full_name, designation)
-VALUES (2, 'Sita Sharma', 'Transport Officer');
 
 SELECT * FROM officers;
 
@@ -65,12 +55,6 @@ CREATE TABLE vehicles (
 
 
 
-INSERT INTO vehicles (owner_id, plate_no, vehicle_type, engine_no, chassis_no)
-VALUES (1, 'BA-2-PA-1234', 'Bike', 'ENG123', 'CHS123');
-
-INSERT INTO vehicles (owner_id, plate_no, vehicle_type, engine_no, chassis_no)
-VALUES (6, 'BA-2-PA-1264', 'Car', 'ENG133', 'CHS128');
-
 
 
 SELECT * FROM vehicles;
@@ -86,14 +70,8 @@ CREATE TABLE bluebooks (
 );
 
 
-INSERT INTO bluebooks (vehicle_id, issue_date, expiry_date, status)
-VALUES (1, '2023-01-01', '2024-01-01', 'EXPIRED');
-
-INSERT INTO bluebooks (vehicle_id, issue_date, expiry_date, status)
-VALUES (16, '2024-01-01', '2025-01-01', 'EXPIRED');
 
 SELECT * FROM bluebooks;
-
 
 
 CREATE TABLE renewals (
@@ -107,14 +85,6 @@ CREATE TABLE renewals (
     FOREIGN KEY (bluebook_id) REFERENCES bluebooks(bluebook_id),
     FOREIGN KEY (officer_id) REFERENCES officers(officer_id)
 );
-
-
-INSERT INTO renewals (bluebook_id, renewal_date, valid_from, valid_to, total_amount)
-VALUES (1, '2024-02-01', '2024-02-01', '2025-02-01', 2500);
-
-
-INSERT INTO renewals (bluebook_id, renewal_date, valid_from, valid_to, total_amount)
-VALUES (2, '2024-07-15', '2024-07-15', '2025-07-15', 3000);
 
 
 
@@ -132,11 +102,6 @@ CREATE TABLE payments (
 
 
 
-INSERT INTO payments (renewal_id, payment_date, amount, payment_method)
-VALUES (1, '2024-02-01', 2500, 'Cash');
-
-INSERT INTO payments (renewal_id, payment_date, amount, payment_method)
-VALUES (3, '2025-02-01', 3000, 'Cash');
 
 SELECT * FROM payments;
 
@@ -153,13 +118,6 @@ SELECT * FROM users;
 SELECT * FROM payments;
 SELECT * FROM renewals;
 
-
--- SELECT v.vehicle_id, v.plate_no, b.bluebook_id, b.expiry_date, b.status
--- FROM vehicles v
--- JOIN bluebooks b ON v.vehicle_id = b.vehicle_id
--- WHERE v.owner_id IN (
---   SELECT owner_id FROM vehicle_owners WHERE user_id = 17
--- );
 
 ALTER TABLE payments
 ADD COLUMN status VARCHAR(20) DEFAULT 'PENDING';
